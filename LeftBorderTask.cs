d81d28cb16e968fa98dd3efae01036797826eb1f
Eatmeta <metagroul@gmail.com>
2022-11-14 22:37:22 +0300

    Implement binary search of the left border in an ordered set of phrases

--- LeftBorderTask.cs
+++ LeftBorderTask.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Autocomplete
 {
@@ -24,13 +23,21 @@ namespace Autocomplete
         {
             // IReadOnlyList похож на List, но у него нет методов модификации списка.
             // Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-            for (int i = 0; i < phrases.Count; i++)
+            /*for (int i = 0; i < phrases.Count; i++)
             {
                 if (string.Compare(prefix, phrases[i], StringComparison.OrdinalIgnoreCase) < 0
                     || phrases[i].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
                     return i - 1;
+                
             }
-            return phrases.Count-1;
+            return phrases.Count-1;*/
+
+            if (right - left == 1) return left;
+            var m = left + (right - left) / 2;
+            if (string.Compare(prefix, phrases[m], StringComparison.OrdinalIgnoreCase) < 0
+                || phrases[m].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                return GetLeftBorderIndex(phrases, prefix, left, m);
+            return GetLeftBorderIndex(phrases, prefix, m, right);
         }
     }
 }
